d0f19e948d9951aeac4519ed9bbdb638492e04c2
corund <kostyapr@mail.ru>
2017-05-15 14:05:34 +0700

    last

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct BST {
     root: Link
@@ -16,10 +17,47 @@ struct Node {
     left: Link,
 }
 
+#[allow(dead_code)]
 impl BST {
-    fn new()->Self
+    pub fn new()->Self
     {
         BST {root: Link::Empty}
     }
+    pub fn insert(&mut self, elem: i32)->bool
+    {
+        let mut result=false;
+        match (*self).root {
+            Link::Empty => 
+                {
+                    let new_node=Node{
+                                    elem: elem,
+                                    left: Link::Empty,
+                                    right: Link::Empty};
+                    (*self).root=Link::More(Box::new(new_node));
+                    result=true;
+                }
+            Link::More(ref boxed_node) => 
+                {
+                    if boxed_node.elem==elem {result=false}
+                    else if boxed_node.elem>elem{}
+                    else if boxed_node.elem<elem{}
+                    result =true;
+                }
+        }
+        result
+    }
+
 }
 
+#[cfg(test)]
+mod test {
+    use super::BST;
+
+    #[test]
+    fn insert_to_empty_tree()
+    {
+        let mut tree = BST::new();
+        tree.insert(34);
+    }
+
+}
